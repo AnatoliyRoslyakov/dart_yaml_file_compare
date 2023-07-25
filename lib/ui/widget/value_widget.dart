eaf97e17e0f9596443ee10ca24caf2c87e8a046b
Anatolii Rosliakov <rosliakov.tolik@yandex.ru>
2023-07-25 23:12:59 +0300

    fixed line-by-line comparison; failed to fix special cases

--- lib/ui/widget/value_widget.dart
+++ lib/ui/widget/value_widget.dart
@@ -12,130 +12,100 @@ class ValueWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ValueBloc, ValueState>(builder: (context, state) {
-      return Row(
-        children: [
-          Container(
-              width: 300,
-              height: double.infinity,
-              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: AppColors.mainElement)),
-              child: state.switchColor
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: ListView.builder(
-                        itemCount: state.lines1.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(1.0),
-                            child: Container(
-                                width: double.infinity,
-                                color: state.colorList1[index],
-                                child: Row(
-                                  children: [
-                                    const SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      '${(index + 1).toString()}: ',
-                                      style: TextStyle(
-                                          color: state.colorList1[index] ==
-                                                  AppColors.orange
-                                              ? Colors.red
-                                              : AppColors.mainElement),
-                                    ),
-                                    const SizedBox(
-                                      width: 15,
-                                    ),
-                                    Text(
-                                      state.lines1[index],
-                                      style: const TextStyle(
-                                        color: AppColors.mainText,
-                                      ),
-                                    ),
-                                  ],
-                                )),
-                          );
-                        },
-                      ),
-                    )
-                  : Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SingleChildScrollView(
-                          child: Text(
-                        state.value1,
-                        style: const TextStyle(color: AppColors.mainText),
-                      )),
-                    )),
-          IconButton(
-              onPressed: () {
-                context.read<ValueBloc>().add(const ValueEvent.switchColor());
-              },
-              icon: state.switchColor
-                  ? const Icon(
-                      Icons.lens,
-                      color: AppColors.mainElement,
-                    )
-                  : const Icon(Icons.panorama_fish_eye,
-                      color: AppColors.mainElement)),
-          Container(
-              width: 300,
-              height: double.infinity,
-              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: AppColors.mainElement)),
-              child: state.switchColor
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: ListView.builder(
-                        itemCount: state.lines2.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(1.0),
-                            child: Container(
-                                width: double.infinity,
-                                color: state.colorList2[index],
-                                child: Row(
-                                  children: [
-                                    const SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      '${(index + 1).toString()}: ',
-                                      style: TextStyle(
-                                          color: state.colorList2[index] ==
-                                                  AppColors.orange
-                                              ? AppColors.red
-                                              : AppColors.mainElement),
-                                    ),
-                                    const SizedBox(
-                                      width: 15,
-                                    ),
-                                    Text(
-                                      state.lines2[index],
-                                      style: const TextStyle(
-                                        color: AppColors.mainText,
-                                      ),
-                                    ),
-                                  ],
-                                )),
-                          );
-                        },
-                      ),
-                    )
-                  : SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          state.value2,
-                          style: const TextStyle(color: AppColors.mainText),
-                        ),
-                      ),
-                    ))
-        ],
-      );
+      return Row(children: [
+        ContentContainer(
+            listLength: state.lines1.length,
+            color: state.colorList1,
+            lines: state.lines1,
+            value: state.value1),
+        ContentContainer(
+            listLength: state.lines2.length,
+            color: state.colorList2,
+            lines: state.lines2,
+            value: state.value1),
+      ]);
     });
+  }
+}
+
+class ContentContainer extends StatelessWidget {
+  final int listLength;
+  final List<Color> color;
+  final List<String> lines;
+  final String value;
+  const ContentContainer({
+    super.key,
+    required this.listLength,
+    required this.color,
+    required this.lines,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minWidth: 250, maxWidth: 750),
+      child: Container(
+          width: 350,
+          height: double.infinity,
+          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: AppColors.mainElement)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    value,
+                    style: const TextStyle(
+                        color: AppColors.mainElement,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Divider(),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: lines.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: Container(
+                            color: color[index],
+                            child: Row(
+                              children: [
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '${(index + 1).toString()}: ',
+                                  style: TextStyle(
+                                      color: color[index] == AppColors.orange
+                                          ? Colors.red
+                                          : AppColors.mainElement),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Text(
+                                  overflow: TextOverflow.ellipsis,
+                                  lines[index],
+                                  style: const TextStyle(
+                                    color: AppColors.mainText,
+                                  ),
+                                ),
+                              ],
+                            )),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
